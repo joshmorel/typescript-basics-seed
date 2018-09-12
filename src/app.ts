2c699dfacc370eb2a0fb9563d7d0a75c09e2876b
Josh Morel <morel.josh@hotmail.com>
2018-09-12 07:31:35 -0400

    es6 demo - non-arrow function this
    context changes - bad in this example

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,14 @@ const pizzas = [{ name: 'Pepporoni', toppings: ['pepperoni'] }];
 const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
 
 console.log(mappedPizzas);
+
+const pizza = {
+  name: 'Blazing Inferno',
+  getName: function() {
+    setTimeout(function () {
+      console.log(this.name); // no good!
+    }, 100)
+  }
+};
+
+pizza.getName();
