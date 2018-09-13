189e56dc31c6f61b900d0ba8544a1dc4cc9f5949
Josh Morel <morel.josh@hotmail.com>
2018-09-13 17:13:35 -0400

    ts demo: use null type

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
-const sing = function(): never {
-  while (true) {
-    console.log('Sha, la, la, la, la, la, la');
-  }
-};
-sing();
+let coupon = 'pizza25';
+
+function removeCoupon(): void {
+  // can only assign to
+  coupon = null;
+}
+
+console.log(coupon);
+
+removeCoupon();
+
+console.log(coupon);
