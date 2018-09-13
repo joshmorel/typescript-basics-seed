cb23988c4488f1584cc671c2ae7c0d904885bd0a
Josh Morel <morel.josh@hotmail.com>
2018-09-13 17:05:18 -0400

    ts demo: annotate never returning fn

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,6 @@
-// function declaration implicitly void in this case
-function orderError(error: string): never {
-  throw new Error(error);
-}
-
-// function expression implicitly never in this case
-var orderErrorExp = function(error: string) {
-  throw new Error(error)
-}
-
-if (Math.random() < 0.5) {
-  orderErrorExp('Expression - Something went wrong')
-} else {
-  orderError('Declaration - Something went wrong')
-}
+const sing = function(): never {
+  while (true) {
+    console.log('Sha, la, la, la, la, la, la');
+  }
+};
+sing();
