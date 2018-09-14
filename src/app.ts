eee655820cbb3d35e4ab6f6b890b440e7409b36e
Josh Morel <morel.josh@hotmail.com>
2018-09-13 20:13:55 -0400

    ts demo: assert type with as
    the NEW way!

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const pizza: Pizza = { name: 'Blazing Inferno', toppings: 5 }
 const serialized = JSON.stringify(pizza);
 
 function getNameFromJSON(obj: string) {
-  return (<Pizza>JSON.parse(obj)).name;
+  return (JSON.parse(obj) as Pizza).name;
 }
 
 const pizzaName = getNameFromJSON(serialized)
